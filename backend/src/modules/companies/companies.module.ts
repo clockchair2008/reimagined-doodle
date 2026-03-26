76f5cf852da8364677d2d0d3909b3bd796337d9d
moin105 <moinlatif105@gmail.com>
2026-03-26 13:54:56 +0500

    ewrwdf

--- backend/src/modules/companies/companies.module.ts
+++ backend/src/modules/companies/companies.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 import { Company } from '../../entities/company.entity';
+import { Invoice } from '../../entities/invoice.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company])],
+  imports: [TypeOrmModule.forFeature([Company, Invoice])],
   controllers: [CompaniesController],
   providers: [CompaniesService],
   exports: [CompaniesService],
